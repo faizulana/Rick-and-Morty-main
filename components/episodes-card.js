d474c70e5fe562d5df7b8d21ffdabef2be375c0f
milana <m.faizulina.sas@gmail.com>
2023-05-28 12:40:43 +0300

    remove date

--- components/episodes-card.js
+++ components/episodes-card.js
@@ -5,7 +5,6 @@ export const EpisodesCard = ({ name, episode, date}) => {
     <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
       <span>{name}</span>
       <span>{episode}</span>
-      <span>{date}</span>
     </div>
   );
 };
